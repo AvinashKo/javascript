172585d2a48b02bd0adecbe7ec2452c8874a3494
AvinashKo <avinashk1616@gmail.com>
2021-01-27 14:15:43 -0600

    decode ways updates

--- src/algorithms/decode-ways/decode-ways.js
+++ src/algorithms/decode-ways/decode-ways.js
@@ -1,32 +1,54 @@
 const isUndefined = require('../util/is-undefined');
 
-let decodeWaysCount = 0;
+const decodeWaysMap = new Map();
 
+// eslint-disable-next-line consistent-return
 const decodeWaysRecursiveFn = (inputStr) => {
+  if (isUndefined(inputStr)) return 0;
   // eslint-disable-next-line no-useless-return
-  if (isUndefined(inputStr) || inputStr.length === 0) return;
-  if (inputStr.substr(0, 1) === '0') decodeWaysRecursiveFn(inputStr.substr(1));
-  else if (inputStr.length === 1) decodeWaysCount += 1;
-  else if (inputStr.length === 2 && +inputStr < 27) {
-    if (+inputStr === 10 || +inputStr === 20) decodeWaysCount += 1;
-    else decodeWaysCount += 2;
-  } else if (inputStr.length === 2 && +inputStr >= 27) decodeWaysCount += 1;
-  else if (inputStr.length > 2) {
-    if (+inputStr.substr(0, 2) >= 27 || inputStr.substr(1, 1) === '0' || inputStr.substr(2, 1) === '0') { decodeWaysRecursiveFn(inputStr.substr(1)); } else {
-      decodeWaysRecursiveFn(inputStr.substr(1));
-      decodeWaysRecursiveFn(inputStr.substr(2));
-    }
+  if (decodeWaysMap.get(inputStr) !== undefined) return decodeWaysMap.get(inputStr);
+  if (inputStr.length === 3 && inputStr.substr(2, 1) === '0') return 1;
+  if (inputStr.length === 2 && inputStr.substr(1, 1) === '0') return 1;
+  if (inputStr.length > 3 && inputStr.substr(2, 1) === '0') {
+    return decodeWaysRecursiveFn(inputStr.substr(3));
+  }
+  if (inputStr.length > 2 && inputStr.substr(1, 1) === '0') {
+    return decodeWaysRecursiveFn(inputStr.substr(2));
+  }
+  if (inputStr.length === 1) return 1;
+  if (inputStr.length === 2 && +inputStr < 27) return 2;
+  if (inputStr.length === 2 && +inputStr >= 27) return 1;
+  if (inputStr.length > 2 && +inputStr.substr(0, 2) >= 27) {
+    return decodeWaysRecursiveFn(inputStr.substr(1));
+  }
+  if (inputStr.length > 2 && +inputStr.substr(0, 2) < 27) {
+    const decodeWays = decodeWaysRecursiveFn(inputStr.substr(1))
+        + decodeWaysRecursiveFn((inputStr.substr(2)));
+    decodeWaysMap.set(inputStr, decodeWays);
+    return decodeWays;
   }
 };
 
+const isUnableToDecode = (inputStr) => {
+  if (inputStr.length === 0) return true;
+  if (inputStr.substr(0, 1) === '0') return true;
+  // eslint-disable-next-line no-plusplus
+  if (inputStr.includes('00')) return true;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < inputStr.length; i++) {
+    if (inputStr.substr(i, 1) === '0') {
+      if (!(inputStr.substr(i - 1, 1) === '1' || inputStr.substr(i - 1, 1) === '2')) { return true; }
+    }
+  }
+  return false;
+};
+
 const decodeWays = (str) => {
-  decodeWaysCount = 0;
-  if (isUndefined(str) || str.length === 0) return 0;
-  // eslint-disable-next-line no-param-reassign
-  while (str.substr(0, 1) === '0') str = str.substr(1);
-  if (str.includes('00')) return 0;
-  decodeWaysRecursiveFn(str);
-  return decodeWaysCount;
+  // invalid input checks
+  if (isUndefined(str)) return 0;
+  // invalid input checks w.r.t zeros
+  if (isUnableToDecode(str)) return 0;
+  return decodeWaysRecursiveFn(str);
 };
 
 module.exports = {
--- src/algorithms/decode-ways/decode-ways.spec.js
+++ src/algorithms/decode-ways/decode-ways.spec.js
@@ -18,13 +18,16 @@ describe('decode ways checks', () => {
       expect(decodeWays.decodeWays('111')).toEqual(3);
     });
     it('should handle leading zeros', () => {
-      expect(decodeWays.decodeWays('0111')).toEqual(3);
+      expect(decodeWays.decodeWays('0111')).toEqual(0);
     });
     it('should handle only zeros', () => {
       expect(decodeWays.decodeWays('00')).toEqual(0);
     });
     it('should handle trailing zeros', () => {
       expect(decodeWays.decodeWays('1110')).toEqual(2);
+    });
+    it('should handle trailing zeros', () => {
+      expect(decodeWays.decodeWays('230')).toEqual(0);
     });
     it('should handle zeros in the middle', () => {
       expect(decodeWays.decodeWays('2101')).toEqual(1);
@@ -33,8 +36,8 @@ describe('decode ways checks', () => {
       expect(decodeWays.decodeWays('10011')).toEqual(0);
     });
     it('should handle larger string', () => {
-      // expect(decodeWays.decodeWays('111111')).toEqual(0);
-      // expect(decodeWays.decodeWays('111111111111111111111111111111111111111111111')).toEqual(0);
+      expect(decodeWays.decodeWays('1111111111')).toEqual(89);
+      expect(decodeWays.decodeWays('111111111111111111111111111111111111111111111')).toEqual(1836311903);
     });
   });
 });
